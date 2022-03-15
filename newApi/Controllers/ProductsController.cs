f6aac3b88ca7252bcb1e64d22b757e1bf41dd104
Hanaa Taha <enghanaa.22@gmail.com>
2022-03-15 07:57:37 +0300

    products

---
+++ newApi/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using newApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace newApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly dbSmartAgricultureContext _context;
+
+        public ProductsController(dbSmartAgricultureContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TblProducts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblProduct>>> GetTblProduct()
+        {
+            return await _context.TblProduct.ToListAsync();
+        }
+
+        // GET: api/TblProducts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TblProduct>> GetTblProduct(int id)
+        {
+            var tblProduct = await _context.TblProduct.FindAsync(id);
+
+            if (tblProduct == null)
+            {
+                return NotFound();
+            }
+
+            return tblProduct;
+        }
+    }
+}
---
+++ newApi/Services/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using newApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace newApi.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
+        Task<AuthModel> GetLoginAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+
+    }
+}
