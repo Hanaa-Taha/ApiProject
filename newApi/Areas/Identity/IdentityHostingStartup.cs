b9f3e2189d84dc66cf84f6160b1b1e9670b2f913
Hanaa Taha <enghanaa.22@gmail.com>
2022-03-19 20:25:19 +0300

    join mvc and api

---
+++ newApi/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using newApi.Model;
+
+[assembly: HostingStartup(typeof(newApi.Areas.Identity.IdentityHostingStartup))]
+namespace newApi.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ newApi/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using newApi.Model;
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace newApi.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+          
+    }
+}
